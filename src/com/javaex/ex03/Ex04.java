0309938b407ca34b6e51800c6f3af3b9f28f26b0
ahah2025 <adghty61>
2025-04-18 18:12:12 +0900

    2nd

--- src/com/javaex/ex03/Ex04.java
+++ src/com/javaex/ex03/Ex04.java
@@ -8,21 +8,21 @@ public class Ex04 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("근무시간:");
+		System.out.print("근무시간:");
 		int time = sc.nextInt();
 		
-		//if{ }를 안쪽에서 생성된 변수는 if들이 끝나면 사라진다
-		//따라서 { }문 밖에서도 사용할 변수는 { }문 밖에서 선언한다. 
+		//if문{ }를 안쪽에서 생성된 변수는 if문이 끝나면 사라진다
+		//따라서 if{ }문 밖에서도 사용할 변수는 if{ }문 밖에서 선언한다.
 		int sum;
 
 		if(time<=8) {
 			sum = time*10000;
 			
 		}else {
-			sum = (time-8)*12000+8*10000;
+			sum = (time-8)*12000 + 8*10000;
 			
 		}
-		System.out.println("월급은 "+sum+"원 입니다." );
+		System.out.println("임금은 "+sum+"원 입니다." );
 		
 		sc.close();
 	}
--- src/com/javaex/ex03/Ex05.java
+++ src/com/javaex/ex03/Ex05.java
@@ -8,9 +8,20 @@ public class Ex05 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("근무시간:"); //철수 아르바이트 1.5배(정수형 말고 실수형)
+		System.out.print("근무시간:");
 		int time = sc.nextInt();
+		
+		double sum;
 
+		if(time <= 8) {
+			sum = time*10000;
+			
+		}else {
+			sum = time*10000 + (time-8)*5000; //철수 편의점 알바 8시간 초과시 시간당 1.5배 받음 (95000.0원)
+		}
+		
+		System.out.println("임금은 "+sum+"원 입니다." );
+		
 		sc.close();
 	}
 
