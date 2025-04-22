2febda7f0969598e404ff33ef49378ca04b2b4e1
ahah2025 <adghty61>
2025-04-22 18:00:16 +0900

    2025-04-22

--- src/com/javaex/ex04/Ex04.java
+++ src/com/javaex/ex04/Ex04.java
@@ -8,15 +8,15 @@ public class Ex04 {
 		
 		//구구단 for문을 활용하여 만들기
 		Scanner sc = new Scanner(System.in);
-		System.out.println("단을 입력해주세요");
-		System.out.print("단 : ");
+		System.out.println("단을 입력해 주세요");
+		System.out.print("단:");
 		
 		int dan = sc.nextInt();
-						
-		for(int i=1; i<=9; i++ ) {
-			System.out.println(dan + "*" + i + "="+ dan * i );
-			
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(dan +"*"+ i + "=" + dan*i );
 		}
+				
 		sc.close();
 		
 	}
