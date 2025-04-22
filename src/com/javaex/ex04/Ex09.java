9305ce750b23d8c2dfd581e690000ed5f117207c
ahah2025 <adghty61>
2025-04-22 17:08:53 +0900

    2025.04.22

--- src/com/javaex/ex04/Ex09.java
+++ src/com/javaex/ex04/Ex09.java
@@ -3,6 +3,7 @@ package com.javaex.ex04;
 public class Ex09 {
 
 	public static void main(String[] args) {
+		/*
 		int no = 1;
 		//6의 배수이자 14의 배수인 가장 적은 정수 찾기
 		
@@ -15,9 +16,9 @@ public class Ex09 {
 			no++;
 		}
 		System.out.println("정답"+no);// 정답 42
+		*/
 		
-		
-		/*  action
+		//action
 		int no = 1;
 		//6의 배수이자 14의 배수인 가장 적은 정수 찾기
 		
@@ -32,7 +33,7 @@ public class Ex09 {
 			no++;
 		}
 		System.out.println("정답"+no);// 정답 43
-		*/
+		
 		
 	}
 
