69ab2c0671f0bed2aff91dd0e092a0b0328bb62b
ahah2025 <adghty61>
2025-04-22 15:17:50 +0900

    2025.04.22

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,25 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		for(int x = 1; x <=6  ; x++ ) {
+			for(var y = 1; y<=6 ; y++ ) {
+				System.out.println("");
+			}
+
+			}
+		}
+	}		
+		
+		
+		/*
+		for(var i=1 ; i<=3 ; i++) {
+			for(var j=0; j < i ; j++) {
+				System.out.println("******") ;
+			}
+		}
+		*/
+	
+
+
