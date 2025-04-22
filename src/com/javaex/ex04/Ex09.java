642463d6d7ac5342396b222475254b6acc42cfea
ahah2025 <adghty61>
2025-04-22 16:47:51 +0900

    2025.04.22

--- src/com/javaex/ex04/Ex09.java
+++ src/com/javaex/ex04/Ex09.java
@@ -5,15 +5,18 @@ public class Ex09 {
 	public static void main(String[] args) {
 		int no = 1;
 		//6의 배수이자 14의 배수인 가장 적은 정수 찾기
-		while( true ) {
+		
+		boolean action = true;
+		
+		while( action ) {
 								
 			if( no%6 == 0 && no%14 ==0) {
-				break;//while문 탈출
+				action=false;//while문 탈출
 			}
 			System.out.println("현재"+no); 
 			no++;
 		}
-		System.out.println("정답"+no);
+		System.out.println("정답"+no);// 정답 43
 	}
 
 }
