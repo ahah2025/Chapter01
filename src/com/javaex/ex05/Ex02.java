8f7ad16127dfd7e315ddc85b26369ea5b746b4be
ahah2025 <adghty61>
2025-04-24 14:52:01 +0900

    3rd

---
+++ src/com/javaex/ex05/Ex02.java
@@ -0,0 +1,26 @@
+package com.javaex.ex05;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		//for문
+		
+		int no01;
+		int no02;
+		int no03;
+		int no04;
+		int no05;
+		int no06;
+		
+		//변수값을 아래와 같이 식으로 표현할 수 없다
+		for(int i=1; i<=6; i++) {
+			no01 = (int)(Math.random()*45)+1; //배열
+			
+		}
+		
+		System.out.println("-----------------------------------------");
+	
+	}
+
+}
