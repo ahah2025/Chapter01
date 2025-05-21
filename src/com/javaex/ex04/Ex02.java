d788a62bcbdde68fc729f6557c1484039974c53a
ahah2025 <adghty61@naver.com>
2025-05-21 11:39:16 +0900

    3rd

--- src/com/javaex/ex04/Ex02.java
+++ src/com/javaex/ex04/Ex02.java
@@ -27,7 +27,7 @@ public class Ex02 {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("단을 입력해주세요");
-		System.out.println("단 : ");
+		System.out.print("단 : ");
 		
 		int dan = sc.nextInt();
 		int i=1; //초기화
@@ -35,7 +35,6 @@ public class Ex02 {
 				
 		while(i<=9) {
 			System.out.println(dan + "*" + (i) + "="+ dan*(i) );
-			
 			i++; // 증감식
 		}
 		sc.close();
